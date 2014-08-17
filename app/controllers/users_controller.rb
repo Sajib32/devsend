d689b1d803adeba49f6cafcf29eb7f474562bdd9
Sajib32 <sajibcste@gmail.com>
2014-08-17 21:20:13 +0000

    initial

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  def index
+    @user = User.all
+  end
+  
+  def create
+    # Create the user from params
+    @user = User.new(params[:user])
+    if @user.save
+      # Deliver the signup email
+      UserNotifier.send_signup_email(@user).deliver
+      redirect_to(@user, :notice => 'User created')
+    else
+      render :action => 'new'
+    end
+  end
+end
